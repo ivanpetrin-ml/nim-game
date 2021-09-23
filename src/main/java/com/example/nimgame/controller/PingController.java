50175ec9641400ebb0b93c3de06cc422c8ee0cda
ipetrin <ivan.petrin@mercadolivre.com>
2021-09-23 14:22:51 -0300

    initial config

---
+++ src/main/java/com/example/nimgame/controller/PingController.java
@@ -0,0 +1,14 @@
+package com.example.nimgame.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PingController {
+
+    @GetMapping("/ping")
+    public String index() {
+        return "pong";
+    }
+
+}
---
+++ src/test/java/com/example/nimgame/NimGameApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.nimgame;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NimGameApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
